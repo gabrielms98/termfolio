a6151e87fba97790c8f8fa0b9f1aed816b779476
Gabriel Martins Software Engineer <gabrielms.dev@gmail.com>
2023-05-05 09:50:29 -0300

    feat: factory;

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
+
 import Draggable from "react-draggable";
 import TerminalWindow from "./TerminalWindow";
 
+interface IAppState {
+  app: JSX.Element;
+  show: boolean;
+}
+
+function Factory(props: { apps: IAppState[] }) {
+  return (
+    <>
+      {props.apps
+        .filter((app) => app.show)
+        .map((app, i) => (
+          <Draggable positionOffset={{ x: i * 5 + "%", y: i * 5 + "%" }}>
+            <div className="absolute">{app.app}</div>
+          </Draggable>
+        ))}
+    </>
+  );
+}
+
 function Window() {
+  const [apps, setApps] = useState<IAppState[]>([
+    { app: <TerminalWindow />, show: true },
+  ]);
+
   return (
     <main className="window w-full h-full relative flex items-center justify-center">
       <div className="w-screen h-screen">
         <div className="z-10 relative w-full h-full flex justify-center items-center">
-          <Draggable>
-            <div className="absolute">
-              <TerminalWindow />
-            </div>
-          </Draggable>
+          <Factory apps={apps} />
         </div>
       </div>
     </main>
